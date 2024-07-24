414cca0a716ac04038e26343d9aaa801b3d8c677
Joe Chirchirillo <cchirchirillo3@gmail.com>
2024-07-24 15:49:39 -0600

    feat: patron class

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -2,12 +2,21 @@ require './lib/patron'
 
 RSpec.describe Patron do
     before(:each) do
+        @patron_1 = Patron.new("Bob, 20")
 
     end
 
-    describe "#initalize" do
+    describe '#initalize' do
         it "exists" do
             expect(patron_1).to be_an_instance_of Patron
+        end
+
+        it 'has a name' do
+
+        end
+
+        it 'has spending money' do
+
         end
 
 
