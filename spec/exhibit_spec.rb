cdaaa645beac811577f5a9483c8092ba9e7cd932
Joe Chirchirillo <cchirchirillo3@gmail.com>
2024-07-24 15:44:33 -0600

    test: can access attributes

--- spec/exhibit_spec.rb
+++ spec/exhibit_spec.rb
@@ -15,6 +15,10 @@ RSpec.describe Exhibit do
             expect(@exhibit.name).to eq "Gems and Minerals"
         end
 
+        it 'has a price' do
+            expect(@exhibit.cost).to eq 0
+        end
+
 
     end
 
