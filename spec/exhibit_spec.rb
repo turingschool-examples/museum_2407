3e44c16a82a30018599501d79acfdb57374d5863
Joe Chirchirillo <cchirchirillo3@gmail.com>
2024-07-24 15:43:28 -0600

    test: initalize

--- spec/exhibit_spec.rb
+++ spec/exhibit_spec.rb
@@ -1,12 +1,23 @@
 require './lib/exhibit'
 
 RSpec.describe Exhibit do
+    before(:each) do
+        @exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
 
-
-    it 'exists' do
-        exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
-
-        expect(exhibit).to be_an_instance_of Exhibit
     end
+
+    describe '#initialize' do
+        it 'exists' do
+            expect(@exhibit).to be_an_instance_of Exhibit
+        end
+
+        it 'has a name' do
+            expect(@exhibit.name).to eq "Gems and Minerals"
+        end
+
+
+    end
+
+
 
 end
