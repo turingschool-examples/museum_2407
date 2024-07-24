cc0a3e86983f132ed369bcadca6463ee5a6ad43b
Joe Chirchirillo <cchirchirillo3@gmail.com>
2024-07-24 15:56:28 -0600

    test: add inteest test suite

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -18,10 +18,23 @@ RSpec.describe Patron do
         it 'has spending money' do
             expect(@patron_1.spending_money).to eq 20
         end
+    end
 
+    describe '#add interests' do
         it 'has interests' do
             expect(@patron_1.interests).to eq []
         end
+
+        it 'can add interests' do
+            expect(@patron_1.interests).to eq []
+
+            @patron_1.add_interest("Dead Sea Scrolls")
+            @patron_1.add_interest("Gems and Minerals")
+
+            expect(@patron_1.interests).to eq ["Dead Sea Scrolls", "Gems and Minerals"]
+        end
+
+
     end
 
 
