6cca0c177281a97146bfb86c570357c9445bf0a5
Joe Chirchirillo <cchirchirillo3@gmail.com>
2024-07-24 16:04:57 -0600

    test: museum initialize test suite

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -7,6 +7,20 @@ RSpec.describe Museum do
         @dmns = Museum.new("Denver Museum of Nature and Science")
     end
 
+    describe 'initalize' do
+        it 'exists' do
+            expect(@dmns).to be_an_instance_of Museum
+        end
+
+        it 'has a name' do
+            expect(@dmns.name).to eq "Denver Museum of Nature and Science"
+        end
+
+        it 'has exhibits' do
+            expect(@dmns.exhibits).to eq []
+        end
+    end
+
 
 
 end
