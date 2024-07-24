a35bf78a5aa47da98b4e1a464ee7794c84303c12
Joe Chirchirillo <cchirchirillo3@gmail.com>
2024-07-24 15:52:14 -0600

    test: expanded initalize test suite

--- lib/patron.rb
+++ lib/patron.rb
@@ -1,9 +1,10 @@
 class Patron
 attr_reader :name, :spending_money
 
-def initialize(name, spending_money)
-    @name = name
-    @spending_money = spending_money
+    def initialize(name, spending_money)
+        @name = name
+        @spending_money = spending_money
+    end
 
 
 end
--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -2,21 +2,21 @@ require './lib/patron'
 
 RSpec.describe Patron do
     before(:each) do
-        @patron_1 = Patron.new("Bob, 20")
+        @patron_1 = Patron.new("Bob", 20)
 
     end
 
     describe '#initalize' do
         it "exists" do
-            expect(patron_1).to be_an_instance_of Patron
+            expect(@patron_1).to be_an_instance_of Patron
         end
 
         it 'has a name' do
-
+            expect(@patron_1.name).to eq "Bob"
         end
 
         it 'has spending money' do
-
+            expect(@patron_1.spending_money).to eq 20
         end
 
 
