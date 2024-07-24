e521b3f76f8e1f0f2ba7cfc293701bb0814a9232
Joe Chirchirillo <cchirchirillo3@gmail.com>
2024-07-24 15:47:10 -0600

    test: patron initalize suite

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -1,1 +1,20 @@
 require './lib/patron'
+
+RSpec.describe Patron do
+    before(:each) do
+
+    end
+
+    describe "#initalize" do
+        it "exists" do
+            expect(patron_1).to be_an_instance_of Patron
+        end
+
+
+    end
+
+
+
+
+
+end
