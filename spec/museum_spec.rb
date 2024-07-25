e09d07434890528f515d93d2fab31f5042c181bb
Kyle Delaney <khdelaney99@gmail.com>
2024-07-25 09:37:40 -0400

    add test for #initialize() method for museum

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -1,13 +1,26 @@
-require './spec_helper'
+require 'spec_helper'
 
 RSpec.configure do |config| 
     config.formatter = :documentation 
-   end
+end
    
-   RSpec.describe Museum do 
-       before(:each) do
-       end
-       describe '#initialize()' do
-           
-       end
-   end
+RSpec.describe Museum do 
+    before(:each) do
+        @dmns = Museum.new("Denver Museum of Nature and Science")
+    end
+
+    describe '#initialize()' do
+        it 'creates an instance of an Patron object' do 
+            expect(@dmns).to be_a Museum
+        end
+
+        it 'has a name passed in as an argument' do
+            expect(@dmns.name).to eq ("Denver Museum of Nature and Science")
+        end
+
+        it 'has an exhibits attribute set to hold an empty array' do
+            expect(@dmns.exhibits).to be_a (Array)
+            expect(@dmns.exhibits.empty?).to be_a (true)
+        end
+    end
+end
