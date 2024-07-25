702e98b3f4fbf7b85c3379d69447a2f42fa2cf63
Jim Macur <jim.macur@gmail.com>
2024-07-25 16:00:18 -0600

    test: can add exhibits to museum

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -9,6 +9,9 @@ end
 RSpec.describe Museum do
     before(:each) do
         @dmns = Museum.new("Denver Museum of Nature and Science")
+        @gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+        @dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+        @imax = Exhibit.new({name: "IMAX",cost: 15})
     end
 
     describe 'instantiate' do
@@ -23,6 +26,14 @@ RSpec.describe Museum do
         it 'has exhibits as an empty array' do
             expect(@dmns.exhibits).to eq []
         end
+    end
+
+    it 'can add exhibits' do
+        @dmns.add_exhibit(@gems_and_minerals)
+        @dmns.add_exhibit(@dead_sea_scrolls)
+        @dmns.add_exhibit(@imax)
+
+        expect(@dmns.exhibits).to eq [@gems_and_minerals, @dead_sea_scrolls, @imax]
     end
 
 end
