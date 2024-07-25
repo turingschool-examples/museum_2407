816d90f37fdecdd8c5c5b8f6903c1572c27a5786
Kyle Delaney <khdelaney99@gmail.com>
2024-07-25 09:45:59 -0400

    add test for #add_exhibit() method for museum

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -25,7 +25,15 @@ RSpec.describe Museum do
     end
 
     describe '#add_exhibit()' do
-    
+        it 'should add the argument to the exhibits array' do
+            gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+            # dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+            # imax = Exhibit.new({name: "IMAX",cost: 15})
+            @dmns.add_exhibit(gems_and_minerals)
+
+            expect{@dmns.add_exhibit}.to_change{@dmns.exhibits.empty?}.from(true).to(false)
+            expect(@dmns.exhibits[0][:name]).to eq ("Gems and Minerals")
+        end
     end
     
     describe '#recommend_exhibits()' do
