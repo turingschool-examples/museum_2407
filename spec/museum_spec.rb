23d9fa1d8eabf7a8d8d302f3a62987ac1bff09bc
Kyle Delaney <khdelaney99@gmail.com>
2024-07-25 09:51:18 -0400

    pass test for #add_exhibit() method for museum

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -29,10 +29,10 @@ RSpec.describe Museum do
             gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
             # dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
             # imax = Exhibit.new({name: "IMAX",cost: 15})
-            @dmns.add_exhibit(gems_and_minerals)
+            # @dmns.add_exhibit(gems_and_minerals)
 
-            expect{@dmns.add_exhibit}.to_change{@dmns.exhibits.empty?}.from(true).to(false)
-            expect(@dmns.exhibits[0][:name]).to eq ("Gems and Minerals")
+            expect{@dmns.add_exhibit(gems_and_minerals)}.to change{@dmns.exhibits.empty?}.from(true).to(false)
+            expect(@dmns.exhibits[0].name).to eq ("Gems and Minerals")
         end
     end
     
