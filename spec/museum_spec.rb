82b077a4cea1edd3a3215d4d4fbfd74fbd2bc181
Jim Macur <jim.macur@gmail.com>
2024-07-25 16:03:01 -0600

    test: adding to the before each

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -9,9 +9,18 @@ end
 RSpec.describe Museum do
     before(:each) do
         @dmns = Museum.new("Denver Museum of Nature and Science")
+        
         @gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
         @dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
         @imax = Exhibit.new({name: "IMAX",cost: 15})
+
+        @patron_1 = Patron.new("Bob", 20)
+        @patron_1.add_interest("Dead Sea Scrolls")
+        @patron_1.add_interest("Gems and Minerals")
+        
+        @patron_2 = Patron.new("Sally", 20)
+        @patron_2.add_interest("IMAX")
+
     end
 
     describe 'instantiate' do
