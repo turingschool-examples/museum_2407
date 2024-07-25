5ed5c4fe474a879aa1594fbac7c38bc7a444a0e0
Kyle Delaney <khdelaney99@gmail.com>
2024-07-25 10:03:36 -0400

    add tests for #recommended_exhibits() method in museum

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -41,6 +41,29 @@ RSpec.describe Museum do
     end
     
     describe '#recommend_exhibits()' do
+        it 'will return an array of exhibits based on the argument/Patrons interests' do
+            gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+            dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+            imax = Exhibit.new({name: "IMAX",cost: 15})
+            patron_1 = Patron.new("Bob", 20)
+            patron_2 = Patron.new("Julia", 400)
+            expect(@dmns.recommend_exhibits(patron_1)).to eq ([])
+
+            patron_1.add_interest("Gems and Minerals")
+            patron_2.add_interest("Dead Sea Scrolls")
+            patron_2.add_interest("IMAX")
+
+            expect(@dmns.recommend_exhibits(patron_1).length).to eq (1)
+            expect(@dmns.recommend_exhibits(patron_2).length).to eq (2)
+            expect(@dmns.recommend_exhibits(patron_2).length).to eq (2)
+
+
+
+        end
+
+        it 'should not add arguments if they are not Patron objects' do
+
+        end
 
     end
 end
