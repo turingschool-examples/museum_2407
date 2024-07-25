e764327ba8d2038dc0e19c74f4157cb4db799a5c
Kyle Delaney <khdelaney99@gmail.com>
2024-07-25 08:59:16 -0400

    add tests for #initialize() method for patron

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -2,4 +2,23 @@ require 'spec_helper'
 
 RSpec.configure do |config| 
  config.formatter = :documentation 
- end
+end
+
+RSpec.describe Exhibit do 
+    before(:each) do
+        @patron_1 = Patron.new("Bob", 20)
+    end
+    describe '#initialize()' do
+        it 'creates an instance of an Patron object' do 
+            expect(@exhibit).to be_a Patron
+        end
+
+        it 'holds a name attribute' do
+            expect(@patron.name).to eq ("Bob")
+        end
+        
+        it 'holds a money_spent attribute' do
+            expect(@patron.money_spent).to eq (20)
+        end
+    end
+end
