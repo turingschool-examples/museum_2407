a9046ddb052a5a264e428013ed07c9476c4b9f7c
Jim Macur <jim.macur@gmail.com>
2024-07-25 15:51:37 -0600

    feat: museum and spec files

---
+++ spec/museum_spec.rb
@@ -0,0 +1,12 @@
+require './lib/museum'
+require './lib/exhibit'
+require './lib/patron'
+
+RSpec.configure do |config|
+    config.formatter = :documentation
+end
+
+RSpec.describe Museum do
+
+    
+end
