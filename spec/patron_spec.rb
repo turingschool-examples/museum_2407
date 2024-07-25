0b2dd666c3f910a03dc4dbadeae6bfff109db60c
Jim Macur <jim.macur@gmail.com>
2024-07-25 15:41:08 -0600

    test: Patron exists

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -1,10 +1,19 @@
- require './lib/exhibit'
+require './lib/exhibit'
 require './lib/patron'
 
 RSpec.configure do |config|
     config.formatter = :documentation
 end
 
-RSpec.describe do
+RSpec.describe Patron do
+    before(:each) do 
+        @patron_1 = Patron.new("Bob", 20)
+    end
+    
+    describe 'instantiate' do
+        it 'exists' do
+            expect(@patron_1).to be_an_instance_of Patron
+        end
+    end
     
 end
