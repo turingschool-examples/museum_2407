4989279201d625111f943c79e7e75a2abdac75f2
Kyle Delaney <khdelaney99@gmail.com>
2024-07-25 08:54:19 -0400

    add tests for #initialize() for exhibit

--- spec/exhibit_spec.rb
+++ spec/exhibit_spec.rb
@@ -5,7 +5,20 @@ RSpec.configure do |config|
 end
 
 RSpec.describe Exhibit do 
+    before(:each) do
+       @exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    end
     describe '#initialize()' do
-        it ''
+        it 'creates an instance of an Exhibit object' do 
+            expect(@exhibit).to be_a Exhibit
+        end
+
+        it 'holds a hash containing a name KVP' do
+            expect(@exhibit.name).to eq ("Gems and Minerals")
+        end
+        
+        it 'holds a hash containing a cost KVP' do
+            expect(@exhibit.cost).to eq (0)
+        end
     end
 end
