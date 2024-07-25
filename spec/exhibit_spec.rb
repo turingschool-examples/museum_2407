0d61668e831a4d1ed9a5e5eaf75822667a84febb
Jim Macur <jim.macur@gmail.com>
2024-07-25 15:32:54 -0600

    test: exhibit has a name

--- spec/exhibit_spec.rb
+++ spec/exhibit_spec.rb
@@ -7,12 +7,16 @@ end
 
 RSpec.describe Exhibit do
     before(:each) do
-        exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+        @exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
     end
     
     describe 'instantiate' do
         it 'exists' do
             expect(@exhibit).to be_an_instance_of Exhibit
+        end
+
+        it 'has a name' do
+            expect(@exhibit.name).to eq "Gems and Minerals"
         end
     end
 end
