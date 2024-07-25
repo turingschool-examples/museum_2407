84b7b13884b1bcec3379ffb6d8991cbbc085c365
Jim Macur <jim.macur@gmail.com>
2024-07-25 15:48:41 -0600

    test: can add intersts

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -27,5 +27,12 @@ RSpec.describe Patron do
             expect(@patron_1.interests).to eq []
         end
     end
+
+    it 'can add interests' do
+        @patron_1.add_interest("Dead Sea Scrolls")
+        @patron_1.add_interest("Gems and Minerals")
+
+        expect(@patron_1.interests).to eq ["Dead Sea Scrolls", "Gems and Minerals"]
+    end
     
 end
