e5600f3d7296d055d1d1d8c2ed8bd86fbffe83c1
Kyle Delaney <khdelaney99@gmail.com>
2024-07-25 09:09:18 -0400

    pass interests in #initialize() method test for patron

--- lib/patron.rb
+++ lib/patron.rb
@@ -1,9 +1,11 @@
 class Patron
     attr_reader :name,
-                :money_spent
+                :money_spent,
+                :interests
 
     def initialize(name, money_spent)
         @name = name
         @money_spent = money_spent
+        @interests = []
     end
 end
