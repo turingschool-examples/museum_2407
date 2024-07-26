8332a9683b9b10185bc808a1d7d863525352f5e3
Kyle Delaney <khdelaney99@gmail.com>
2024-07-26 09:20:14 -0400

    add tests for #patron_by_exhibit_interest method in museum

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -72,12 +72,45 @@ RSpec.describe Museum do
         it 'returns a Hash' do
             expect(@dmns.patrons_by_exhibit_interest).to be_a (Hash)
         end
-        
-        it 'shows each Exhibit as its own key' do
 
+        it 'shows each Exhibit as its own key' do
+            gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+            dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+            imax = Exhibit.new({name: "IMAX",cost: 15})
+            @dmns.add_exhibit(gems_and_minerals)
+            @dmns.add_exhibit(dead_sea_scrolls)
+            @dmns.add_exhibit(imax)
+
+            expect(@dmns.patrons_by_exhibit_interest.keys.length).to eq (3)
+            expect(@dmns.patrons_by_exhibit_interest.keys[0].name).to eq ("Gems and Minerals")
+            expect(@dmns.patrons_by_exhibit_interest.keys[1].name).to eq ("Dead Sea Scrolls")
+            expect(@dmns.patrons_by_exhibit_interest.keys[2].name).to eq ("IMAX")
+            
         end
         it 'each value is an Array of Patron objects who are interested in the Exhibit key' do
+            gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+            dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+            imax = Exhibit.new({name: "IMAX",cost: 15})
+            @dmns.add_exhibit(gems_and_minerals)
+            @dmns.add_exhibit(dead_sea_scrolls)
+            @dmns.add_exhibit(imax)
 
+            patron_1 = Patron.new("Bob", 20)
+            patron_2 = Patron.new("Julia", 400)
+            patron_3 = Patron.new("Chandler", 0)
+
+            patron_1.add_interest(gems_and_minerals)
+
+            patron_2.add_interest(gems_and_minerals)
+            patron_2.add_interest(dead_sea_scrolls)
+            
+            patron_3.add_interest(gems_and_minerals)
+            patron_3.add_interest(dead_sea_scrolls)
+            patron_3.add_interest(imax)
+
+            expect(@dmns.patrons_by_exhibit_interest.values[0].length).to eq (3)
+            expect(@dmns.patrons_by_exhibit_interest.values[1].length).to eq (2)
+            expect(@dmns.patrons_by_exhibit_interest.values[2].length).to eq (1)
         end
     
     end
